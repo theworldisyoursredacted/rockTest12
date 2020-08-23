2be8427c63316b73f61d11ac22bf5831c9b129e1
Jake Hamblin <jekeltor44@gmail.com>
2020-08-22 22:13:25 -0400

    Added files for repository

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+	/////////////////////////////////////////////////////////////////////////
+	//                                                                     //
+	//                       Created by Jake Hamblin                       //
+    //                           jakehamblin.com                           //
+	//                                                                     //
+	//  FOR ALL THE VALUES, MAKE SURE TO KEEP THE QUOTATIONS AROUND THEM!  //
+	//                                                                     //
+	/////////////////////////////////////////////////////////////////////////
+
+	// General Site Information
+	$name = "Jake Hamblin"; // Name you want to appear throughout the website. EX: Jake Hamblin
+	$domain = "https://projects.jakehamblin.com/splashpage"; // The full link to your domain including HTTP. EX: https://jakehamblin.com
+	$logo = "https://jakehamblin.com/images/logo.png"; // Link to the logo to appear throughout the website. EX: https://jakehamblin.com/images/logo.png
+	$color = "3fa3eb"; // Color you want to use throughout the website in HEX form excluding the hashtag. EX: 3fa3eb
+	$description = "Software programmer and website developer"; // Description of the website that will appear in the embed. EX: Software programmer and website developer
+	$twitter = "jekeltor"; // Twitter of the website excluding the at sign. EX: jekeltor
+	$gradientcolor1 = "142430"; // Color you want to use as the first color of the gradient in HEX form excluding the hashtag. EX: 142430
+	$gradientcolor2 = "0c151c"; // Color you want to use as the second color of the gradient in HEX form excluding the hashtag. EX: 0c151c
+
+	// Social Media with the link as the key and the FontAwesome class as the data. You can find the FontAwesome class at https://fontawesome.com
+	$socialmedia = [
+		"https://jakehamblin.com/twitter" => "fab fa-twitter",
+		"https://jakehamblin.com/youtube" => "fab fa-youtube",
+		"https://jakehamblin.com/discord" => "fab fa-discord",
+		"https://jakehamblin.com/instagram" => "fab fa-instagram",
+		"https://jakehamblin.com/codepen" => "fab fa-codepen",
+		"https://jakehamblin.com/github" => "fab fa-github",
+	]
+?>
